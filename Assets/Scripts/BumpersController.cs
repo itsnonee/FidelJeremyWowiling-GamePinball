3f9600f2229c6327a1ed319ab3ed5efd34431a1e
itsnonee <fidelwo05@gmail.com>
2024-02-26 11:05:37 +0800

    Add Material
    Menambahkan material warna untuk bumpers

--- Assets/Scripts/BumpersController.cs
+++ Assets/Scripts/BumpersController.cs
@@ -6,6 +6,12 @@ public class BumpersController : MonoBehaviour
 {
     [SerializeField] private Collider ball;
     [SerializeField] private float multiplier;
+    [SerializeField] private Color color;
+
+    private void Start()
+    {
+        GetComponent<Renderer>().material.color = color;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider == ball)
