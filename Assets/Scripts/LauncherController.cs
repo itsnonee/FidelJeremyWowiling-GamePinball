74e851ce791e838be0d1d0e685f755f5568ee6ac
itsnonee <fidelwo05@gmail.com>
2024-02-27 20:35:04 +0800

    Add LauncherController
    Menambahkan LauncherController untuk mengatur dorongan launcher kepada bola pada saat mulai permainan

---
+++ Assets/Scripts/LauncherController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherController : MonoBehaviour
+{
+    [SerializeField] private Collider bola;
+    [SerializeField] private KeyCode input;
+    [SerializeField] private float maxForce;
+    [SerializeField] private float maxTimeHold;
+
+    private bool isHold = false;
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider == bola)
+        {
+            Debug.Log("Bola dah stay");
+            ReadInput(bola);
+        }
+    }
+
+    void ReadInput(Collider collider)
+    {
+        if (Input.GetKey(input) && !isHold)
+        {
+            StartCoroutine(StartHold(collider));
+        }
+    }
+
+    private IEnumerator StartHold(Collider collider)
+    {
+        isHold = true;
+
+        float force = 0.0f;
+        float timeHold = 0.0f;
+
+        while (Input.GetKey(input))
+        {
+            force = Mathf.Lerp(0, maxForce, timeHold/maxTimeHold);
+
+            yield return new WaitForEndOfFrame();
+            timeHold += Time.deltaTime;
+        }
+        collider.GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
+        isHold = false;
+        
+    }
+}
